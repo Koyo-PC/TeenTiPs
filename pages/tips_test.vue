2a4b314330ab73ae59a0c3df07aec62640980de3
ZOI-dayo <64856587+ZOI-dayo@users.noreply.github.com>
2021-07-13 15:33:30 +0900

    V-Forのサンプルとして

---
+++ pages/tips_test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="data in jsonData">
+      <span class="name">{{ data.name }}</span>
+      <span class="content">{{ data.content }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "tips_test.vue",
+  data() {
+    return {
+      jsonData: [
+        {
+          "name": "",
+          "content": ""
+        }
+      ]
+    }
+  },
+  created: function () {
+    axios.get('b.json')
+      .then(response => {
+        let json = response.data;
+        this.jsonData = json;
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+}
+</script>
