9b8d59b3e274ac7fa0fb58275c7ea53ddfde2de4
MinamideTakaaki <minamide.takataka@gmail.com>
2022-05-26 23:07:23 +0900

    画面構成

---
+++ components/Title.vue
@@ -0,0 +1,30 @@
+<template>
+	<div style="width: 80%;">
+		<h2>【Girl in STEM】理系分野での女性の可能性を広げるために。宇宙飛行士・山崎直子さんと考える進路のエンパワメント</h2>
+		<span>宇宙に興味がある女の子は多くても、キャリアや進路で選択する人はごく僅か。そのジェンダーギャップの要因はどこに？</span>
+		<p>2021/10/19</p>
+	</div>
+</template>
+
+<style scoped>
+	div {
+		/*width: 80%;*/
+		margin: 5vh auto 0;
+	}
+
+	h2 {
+		/*font-family: serif;*/
+		/*color: blue;*/
+		font-size: 40px;
+		margin: 0;
+	}
+
+	span {
+		font-size: 20px;
+	}
+
+	p {
+		margin: 5px 0 0 10px;
+		font-size: 14px;
+	}
+</style>
