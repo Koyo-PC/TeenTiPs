e009b9e05f9df78162938c98114d9a33e09262a2
MinamideTakaaki <minamide.takataka@gmail.com>
2022-05-26 23:07:03 +0900

    article構成

--- pages/article.vue
+++ pages/article.vue
@@ -1,13 +1,34 @@
 <template>
 	<div>
 		<Top />
-			<div>ああああ</div>
+		<Title />
+		
 	</div>
 </template>
 
 <script>
 export default {
-  name: "tips_test.vue"
+  name: "tips_test.vue",
+  data() {
+    return {
+      jsonData: [
+        {
+          "name": "",
+          "content": ""
+        }
+      ]
+    }
+  },
+  created: function () {
+    axios.get('b.json')
+      .then(response => {
+        let json = response.data;
+        this.jsonData = json;
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
 }
 </script>
 
