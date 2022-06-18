a9befd1a3d4cb4e98486fea813f25c4d2176c60b
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-06-18 16:27:14 +0900

    Add temp md support

---
+++ pages/articles/_name.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<Top />
+		<Title :data="titleData" />
+    <div v-html="content"></div>
+	</div>
+</template>
+
+<script>
+import {marked} from "marked";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      titleData: {},
+      content: ""
+    }
+  },
+  async mounted() {
+    const mdContent = (await axios.get(`/articles/${this.$route.params.name}.md`)).data;
+    const htmlContent = marked.parse(mdContent)
+    this.content = htmlContent;
+  }
+}
+</script>
+
+<style>
+	html, body {
+		width: 100%;
+		height: 100%;
+		margin: unset;
+	}
+</style>
