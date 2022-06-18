eae298c0b6d13e8797825a7c1c43939a38ea42d8
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-06-18 16:28:14 +0900

    Change Project name

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Koyo_Tips',
+    title: 'TeenTiPs',
     htmlAttrs: {
       lang: 'en'
     },
