80d6c89159d816e7473bff5c1961319e1e763862
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-06-19 22:49:42 +0900

    Matsubara + Matsuo

---
+++ static/articles/matsubara.js
@@ -0,0 +1,24 @@
+() => ({
+  name: "松原仁",
+  sys_name: "matsubara",
+  thumbnail: "/logo.png",
+  color: "#fff",
+  title: {
+    text: "松原仁",
+    subtitle: "AI",
+    date: "1970/1/1"
+  },
+  profile: {
+    content: `1959年東京生まれ。1981年東大理学部情報科学科卒業。1986年同大学院工学系研究科情報工学専攻博士課程修了。同年電子技術総合研究所（現産業技術総合研究所）入所。2000年公立はこだて未来大学システム情報科学部教授。2020年東京大学大学院教授。著書に「人工知能に哲学は必要か」、「将棋とコンピュータ」、「コンピュータ将棋の進歩シリーズ」、「鉄腕アトムは実現できるか」、「ロボットの情報学」、「先を読む頭脳」など。
+（参考）https://www.cyber-u.ac.jp/teacher/matsubara_hitoshi.html`,
+    photo: "/logo.png"
+  },
+  relative: [
+    "katsura",
+    "hiroyuki",
+    "katsura",
+    "hiroyuki",
+    "katsura",
+    "hiroyuki"
+  ]
+})
