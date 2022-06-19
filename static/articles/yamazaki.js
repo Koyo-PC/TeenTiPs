55b5706f7f5af1be7de06ee22c2935e6942e4c1f
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-06-19 10:27:09 +0900

    Add Ms.Yamazaki

---
+++ static/articles/yamazaki.js
@@ -0,0 +1,21 @@
+() => ({
+  title: {
+    text: "山崎直子",
+    sub: "宇宙飛行士",
+    date: "1970/1/1"
+  },
+  profile: {
+    title: "山崎直子さんのプロフィール",
+    content: `1970年千葉県松戸市で生まれた。お茶の水女子大学附属高等学校卒業後、東京大学工学部航空学科に進学。1996年、東京大学大学院航空宇宙工学専攻修士課程修了。
+1999年、宇宙飛行士候補者に選抜され、2001年、宇宙飛行士として認定、2010年、スペースシャトル「ディスカバリー号」によるSTS-131ミッションのミッションスペシャリスト(MS)として参加した。現在は、宇宙航空研究開発機構(JAXA)を退職し内閣府宇宙政策委員会委員や一般社団法人Space Port Japan代表理事に従事する。
+（参考）https://humans-in-space.jaxa.jp/space-job/astronaut/yamazaki-naoko/`
+  },
+  relative: [
+    "katsura",
+    "hiroyuki",
+    "katsura",
+    "hiroyuki",
+    "katsura",
+    "hiroyuki"
+  ]
+})
