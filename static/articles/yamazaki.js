86f1588cf380256c2d4d11c724eb25b5109cc47f
MinamideTakaaki <minamide.takataka@gmail.com>
2022-06-21 00:48:25 +0900

    add 山崎写真

--- static/articles/yamazaki.js
+++ static/articles/yamazaki.js
@@ -10,6 +10,6 @@
     content: `1970年千葉県松戸市で生まれた。お茶の水女子大学附属高等学校卒業後、東京大学工学部航空学科に進学。1996年、東京大学大学院航空宇宙工学専攻修士課程修了。
 1999年、宇宙飛行士候補者に選抜され、2001年、宇宙飛行士として認定、2010年、スペースシャトル「ディスカバリー号」によるSTS-131ミッションのミッションスペシャリスト(MS)として参加した。現在は、宇宙航空研究開発機構(JAXA)を退職し内閣府宇宙政策委員会委員や一般社団法人Space Port Japan代表理事に従事する。
 （参考）https://humans-in-space.jaxa.jp/space-job/astronaut/yamazaki-naoko/`,
-    photo: "/logo.png"
+    photo: "/images/yamazaki/2.webp"
   }
 })
